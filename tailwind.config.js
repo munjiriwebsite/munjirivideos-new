ad3bb483c3c3f3f09d719d0e161985671cac942f
Nyoni Safaris <monkeygenesafaris@gmail.com>
2026-01-13 08:42:15 +0000

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,19 @@ export default {
   
  content: [
     "./index.html",
-     "./blog.html",
-    "./contact.html",
-    "./video-content-packages.html",
     "./video-production-portfolio.html",
     "./creative-video-productions.html",
     "./charity-video-production.html",
     "./nature-video-production.html",
+    "./blog.html",
+    "./video-content-packages.html",
+    "./contact.html",
     "./video-production-cape-town.html",
     "./video-production-kenya.html",
     "./video-production-portugal.html",
     "./video-production-south-africa.html",
     "./videographer-portugal.html",
+    "./african-bat-conservation-munjiri-videos.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
--- vite.config.js
+++ vite.config.js
@@ -8,18 +8,19 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        contact: path.resolve(__dirname, 'contact.html'),
-        blog: path.resolve(__dirname, 'blog.html'),
-        services: path.resolve(__dirname, 'video-content-packages.html'),
         portfolio: path.resolve(__dirname, 'video-production-portfolio.html'),
         creativevideoproductions: path.resolve(__dirname, 'creative-video-productions.html'),
         charityvideoproduction: path.resolve(__dirname, 'charity-video-production.html'),
         naturevideoproduction: path.resolve(__dirname, 'nature-video-production.html'),
+        blog: path.resolve(__dirname, 'blog.html'),
+        services: path.resolve(__dirname, 'video-content-packages.html'),
+        contact: path.resolve(__dirname, 'contact.html'),
         videoproductioncapetown: path.resolve(__dirname, 'video-production-cape-town.html'),
         videoproductionkenya: path.resolve(__dirname, 'video-production-kenya.html'),
         videoproductionportugal: path.resolve(__dirname, 'video-production-portugal.html'),
         videoproductionsouthafrica: path.resolve(__dirname, 'video-production-south-africa.html'),
         videographerportugal: path.resolve(__dirname, 'videographer-portugal.html'),
+        africanbatconservationmunjirivideos: path.resolve(__dirname, 'african-bat-conservation-munjiri-videos.html'),
       }
     }
   }
