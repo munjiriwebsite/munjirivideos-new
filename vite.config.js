2986e71e72bfb680eae64c3210442fb08b39946f
Nyoni Safaris <monkeygenesafaris@gmail.com>
2026-01-13 14:28:53 +0000

    changes

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,22 @@ export default defineConfig({
         blog: path.resolve(__dirname, 'blog.html'),
         services: path.resolve(__dirname, 'video-content-packages.html'),
         contact: path.resolve(__dirname, 'contact.html'),
+        africanbatconservationmunjirivideos: path.resolve(__dirname, 'african-bat-conservation-munjiri-videos.html'),
+        brandstoryvideoguide: path.resolve(__dirname, 'brand-story-video-guide.html'),
+        brandvideoproduction: path.resolve(__dirname, 'brand-video-production.html'),
+        brandvideos: path.resolve(__dirname, 'brand-videos.html'),
+        charityvideographer: path.resolve(__dirname, 'charity-videographer.html'),
+        charlestonhouse: path.resolve(__dirname, 'charleston-house.html'),
+        coimbravideoproduction: path.resolve(__dirname, 'coimbra-video-production.html'),
+        collingwoodnorrisknitwearmunjirivideos: path.resolve(__dirname, 'collingwood-norris-knitwear-munjiri-videos.html'),
+        commonmistakessocialmediavideoproduction: path.resolve(__dirname, 'common-mistakes-social-media-video-production.html'),
+        dehyecollectivecapetown: path.resolve(__dirname, 'dehye-collective-cape-town.html'),
+        dronevideographerportugal: path.resolve(__dirname, 'drone-videographer-portugal.html'),
         videoproductioncapetown: path.resolve(__dirname, 'video-production-cape-town.html'),
         videoproductionkenya: path.resolve(__dirname, 'video-production-kenya.html'),
         videoproductionportugal: path.resolve(__dirname, 'video-production-portugal.html'),
         videoproductionsouthafrica: path.resolve(__dirname, 'video-production-south-africa.html'),
         videographerportugal: path.resolve(__dirname, 'videographer-portugal.html'),
-        africanbatconservationmunjirivideos: path.resolve(__dirname, 'african-bat-conservation-munjiri-videos.html'),
       }
     }
   }
